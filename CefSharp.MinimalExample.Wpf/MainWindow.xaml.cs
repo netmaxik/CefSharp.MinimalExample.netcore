ea7d94ff293a9f0a9982cf8e26caf7ba403e609a
Maxim <netmaxik@gmail.com>
2024-02-01 11:19:56 +1000

    Add project files.

---
+++ CefSharp.MinimalExample.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Data.Common;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace CefSharp.MinimalExample.Wpf
+{
+    public partial class MainWindow : Window
+    {
+        public ICommand TestWork { get; set; }
+        private bool Show_Coords = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+            TestWork = new RelayCommand(TestBugCommand);
+            DataContext = this; // Set the data context for binding
+        }
+
+        private void TestBugCommand()
+        {
+            Show_Coords = !Show_Coords;
+
+        }
+
+        /// <summary>
+        /// Do script  - making EvaluateScriptAsync
+        /// </summary>
+        /// <param name="script"></param>
+        /// <returns></returns>        
+        private string Do_Script (string script)
+        {
+            int timeout = 10;
+            string res = "";
+            IFrame workframe = Browser.GetFocusedFrame();
+            try
+            {
+                var task = workframe.EvaluateScriptAsync(script, "", 1, TimeSpan.FromSeconds(timeout));
+                var complete = task.ContinueWith(t =>
+                {
+                    if (!t.IsFaulted)
+                    {
+                        if (t.Status != TaskStatus.RanToCompletion)
+                        {
+                            res= "not finished";
+                        }
+                        else
+                        {
+                            var response = t.Result;
+                            res = Convert.ToString(response.Success ? (response.Result ?? "null") : response.Message);
+                        }
+                    }
+                }, TaskScheduler.Default);
+
+                complete.Wait();
+            }
+            catch (Exception)
+            {
+
+                res = "error";
+            }
+            return res;
+        }
+
+        /// <summary>
+        /// Window_MouseMove - on mounse show coords
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!Show_Coords)
+                return;
+
+            var X = e.GetPosition(Browser).X;
+            var Y = e.GetPosition(Browser).Y;
+            string scr = "(function() { return document.elementFromPoint(" + X.ToString() + "," + Y.ToString() + @").outerHTML })();";
+            var res = Do_Script(scr);
+
+            if (string.IsNullOrEmpty(res) || string.IsNullOrEmpty(res) || res == "error" || res == "not finished")
+            {
+                return;
+            }
+            
+
+            Title = $"{X}:{Y} - {res}";
+        }
+    }
+
+    public class RelayCommand : ICommand
+    {
+        private Action _execute;
+
+        public RelayCommand(Action execute)
+        {
+            _execute = execute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true; // Here you can put logic to enable or disable the command
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,39 @@
+# CefSharp.MinimalExample
+
+Minimal example of how the CefSharp library can be used using the official `Nuget` packages.
+
+This project includes examples for
+- WinForms
+- WPF
+- OffScreen
+
+| Solution | .Net Version | Description |
+|--------|--------|--------|
+| [CefSharp.MinimalExample.sln](https://github.com/cefsharp/CefSharp.MinimalExample/blob/master/CefSharp.MinimalExample.sln)         | .Net 4.5.2 | Older Non-SDK Style projects that target .Net 4.5.2 and use packages.config |
+| [CefSharp.MinimalExample.net472.sln](https://github.com/cefsharp/CefSharp.MinimalExample/blob/master/CefSharp.MinimalExample.net472.sln)  | .Net 4.7.2 | Newer SDK Style projects that target .Net 4.7.2 and use PackageReference |
+| [CefSharp.MinimalExample.netcore.sln](https://github.com/cefsharp/CefSharp.MinimalExample/blob/master/CefSharp.MinimalExample.netcore.sln) | .Net Core 3.1/5.0/6.0/7.0 | Newer SDK Stlye projects that target .Net Core 3.1, .Net 5.0, .Net 6.0 and .Net 7.0 using PackageReference | 
+ 
+For a more complete example of each project see the main `CefSharp` repository.
+
+## .NET Core support
+
+For **.Net Core 3.1/Net 5/6/7** follow the [Quick Start](https://github.com/cefsharp/CefSharp/wiki/Quick-Start-For-MS-.Net-5.0-or-greater).
+
+**Publish Example**
+```
+# Publish x86
+dotnet publish -f netcoreapp3.1 -r win-x86
+# Publish x64
+dotnet publish -f netcoreapp3.1 -r win-x64
+# Publish using current runtime identifier https://learn.microsoft.com/en-us/dotnet/core/tools/dotnet-publish
+dotnet publish -f netcoreapp3.1 --use-current-runtime
+```
+
+For publishing as single file you need to [Self Host the BrowserSubProcess](https://github.com/cefsharp/CefSharp/wiki/SelfHost-BrowserSubProcess), more details in https://github.com/cefsharp/CefSharp/issues/3407#issuecomment-787008626
+
+Any problems please report them on https://github.com/cefsharp/CefSharp/discussions
+
+## .NET 5/6/7 Support
+
+For **.Net 5/6/7** follow the [Quick Start](https://github.com/cefsharp/CefSharp/wiki/Quick-Start-For-MS-.Net-5.0-or-greater).
+
